8e112fca9fceb5dbd89d151c592280be63f1ef0f
Bhaskar Kumar <bhaskar.kumar@tresvista.net>
2026-04-14 08:55:20 +0000

    fix starlette issue

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html", {"key": "value"})
 
 @app.get("/api/hello")
 def hello():
